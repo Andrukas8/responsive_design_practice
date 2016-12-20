9a27831a38567952a8e93d9a17922b6b1623e849
Andrukas8 <rukasav@gmail.com>
2016-12-20 11:33:41 +0200

    adds FAQ section of the page

---
+++ script.js
@@ -0,0 +1,40 @@
+// hiding and showing answers for FAQs
+var q1 = document.getElementById("question1");
+var a1 = document.getElementById("anaswer1");
+a1.classList.add("invisible");
+q1.addEventListener("click", function () {
+  a1.classList.toggle("invisible");
+});
+
+
+var q2 = document.getElementById("question2");
+var a2 = document.getElementById("anaswer2");
+a2.classList.add("invisible");
+q2.addEventListener("click", function () {
+  a2.classList.toggle("invisible");
+});
+
+
+var q3 = document.getElementById("question3");
+var a3 = document.getElementById("anaswer3");
+a3.classList.add("invisible");
+q3.addEventListener("click", function () {
+  a3.classList.toggle("invisible");
+});
+
+
+
+var q4 = document.getElementById("question4");
+var a4 = document.getElementById("anaswer4");
+a4.classList.add("invisible");
+q4.addEventListener("click", function () {
+  a4.classList.toggle("invisible");
+});
+
+
+var q5 = document.getElementById("question5");
+var a5 = document.getElementById("anaswer5");
+a5.classList.add("invisible");
+q5.addEventListener("click", function () {
+  a5.classList.toggle("invisible");
+});
