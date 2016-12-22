a6980c359d8aeb5a94f835094d709032d0bfb106
Andrukas8 <rukasav@gmail.com>
2016-12-22 11:30:31 +0200

    adds a footer and a cartoon woman

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ function showAnswer(answerId) {
   var currentAnswer = document.getElementById(answerId);
   currentAnswer.classList.toggle("invisible");
 }
+
+//hides the woman when hovering over input tags
+var formItem = document.getElementsByTagName("input");
+var woman = document.getElementsByClassName("woman");
+
+for (i = 0; i < formItem.length; i++) {
+  formItem[i].addEventListener("mouseover", function () {
+    woman[0].classList.add("invisible");
+  });
+}
